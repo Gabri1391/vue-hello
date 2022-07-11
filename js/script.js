bfb0665012a37dd0f7383e85bb2c6f7787a3d07f
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-11 15:39:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const root = new Vue({
+    el: '#root',
+    data: {
+        teachers: 'tutors',
+        firstname: 'Gabriele',
+        secondname: 'Chiarello',
+        classroom: 'Classe 68',
+        agency: 'Boolean',
+        logo: 'https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/aad207b7-a16c-4296-b99a-4a877d7cb755',
+        team: 'A.C.Milan',
+    }
+});
